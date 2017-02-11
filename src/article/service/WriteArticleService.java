5bf95dbba3765cd5f2c9846ebb21620b4326c61f
Yohan Lee <yh0john@gmail.com>
2017-02-11 21:53:37 +0900

    WriteRequest,Service 구ㅕㄴ

--- src/article/service/WriteArticleService.java
+++ src/article/service/WriteArticleService.java
@@ -1,5 +1,67 @@
 package article.service;
 
-public class WriteArticleService {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Date;
 
+import article.dao.ArticleContentDao;
+import article.dao.ArticleDao;
+import article.model.Article;
+import article.model.ArticleContent;
+import jdbc.JdbcUtil;
+import jdbc.connection.ConnectionProvider;
+
+/*
+ * WriteRequest 와 ArticleDao,ArticleContentDao 연결 class
+ * 직접적인 작업 수행 class
+ * 
+ * */
+public class WriteArticleService {
+	private ArticleDao articleDao = new ArticleDao();
+	private ArticleContentDao contentDao = new ArticleContentDao();
+	
+	public Integer write(WriteRequest req){
+		Connection conn = null;
+		try{
+			conn = ConnectionProvider.getConnection();
+			conn.setAutoCommit(false);
+
+			Article article = toArticle(req);
+			Article savedArticle = articleDao.insert(conn, article);
+			
+			//Insert return 값을 활용한 validation check 
+			//wow
+			if(savedArticle ==null){
+				throw new RuntimeException("fail to insert article");
+			}
+			ArticleContent content = new ArticleContent(savedArticle.getNumber(),req.getContent());
+			ArticleContent savedContent = contentDao.insert(conn, content);
+			
+			//Insert return 값을 활용한 validation check
+			//null일 경우 insert fail
+			if(savedContent ==null){
+				throw new RuntimeException("fail to insert article_content");
+			}
+			
+			conn.commit();
+			
+			
+		}catch(SQLException e){
+			JdbcUtil.rollback(conn);
+			throw new RuntimeException(e);
+		}catch(RuntimeException e){
+			JdbcUtil.rollback(conn);
+			throw e;
+		}finally{
+			
+			JdbcUtil.close(conn);
+		}
+		return null;
+		
+	}
+	
+	private Article toArticle(WriteRequest req){
+		Date date =  new Date();
+		return new Article(null,req.getWriter(),req.getTitle(),date,date,0);
+	}
 }
--- src/article/service/WriteRequest.java
+++ src/article/service/WriteRequest.java
@@ -7,6 +7,40 @@ package article.service;
  * 
  * */
 
+import java.util.Map;
+
+import article.model.Writer;
+
 public class WriteRequest {
-	//TODO 폼 내용 만든 뒤 class 메서드 및 멤버 추가
+	private Writer writer;
+	private String title;
+	private String content;
+	
+	public WriteRequest(Writer writer, String title, String content) {
+		super();
+		this.writer = writer;
+		this.title = title;
+		this.content = content;
+	}
+
+	public Writer getWriter() {
+		return writer;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+	//제목이 null 일 경우, validation check 에서 검사
+	public void validate(Map<String,Boolean>errors){
+		if(title==null||title.trim().isEmpty()){
+			errors.put("title",Boolean.TRUE);
+			
+		}
+	}
+	
 }
