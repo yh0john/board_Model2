8b3441215f2e8f84728aa94225b4b4bcd0852b45
Yohan Lee <yh0john@gmail.com>
2017-02-14 21:59:59 +0900

    board Util class 추가

---
+++ src/board/util/MailUtil.java
@@ -0,0 +1,5 @@
+package board.util;
+
+public class MailUtil {
+	//TODO Google STMP Server를 활용한 메일 전송 구현
+}
---
+++ src/board/util/UploadServlet.java
@@ -0,0 +1,5 @@
+package board.util;
+
+public class UploadServlet {
+	//TODO Servlet 3.0을 이용한 파일 업로드 구현 해 보기.
+}
